94a03702ad9942ed190071836cb18ade8578c087
moizghumann <moizghuman@gmail.com>
2023-11-14 00:16:39 +0500

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,20 @@ const App = () => {
   const mapRef = useRef<MapRef>(null);
   const [userInteracting, setUserInteracting] = useState<boolean>(false);
 
+  // function variable that animates the rotation of globe
   const SpinGlobe = () => {
     const map = mapRef.current;
     if (map != null) {
+      // calculates the new center for globe rotation based on map zoom and interaction state.
       const newCenter = calculateRotation({ map, userInteracting });
       if (newCenter) {
+        // initiates a smooth map transition to the new center with a specified duration and easing function.
         map.easeTo({ center: newCenter, duration: 1000, easing: (n) => n });
       }
     }
   }
 
+  // updates the state and calls SpinGlobe function variable based on MapLayerMouseEvent's type
   const handleUserInteraction = (event: MapLayerMouseEvent | ViewStateChangeEvent) => {
     if (event.type === 'mousedown') {
       setUserInteracting(true);
@@ -29,13 +33,19 @@ const App = () => {
     }
   };
 
+  // Handles the click event on the map. If a cluster is clicked, it retrieves the cluster's ID and
+  // triggers the expansion zoom effect, updating the map view to focus on the clicked cluster's center
+  // with a smooth easing motion and adjusted zoom level.
   const onClick = (event: MapLayerMouseEvent) => {
     if (event.features) {
       const feature = event.features[0];
       const clusterId = feature.properties?.cluster_id;
 
+      // retrieves the GeoJSON source named 'earthquakes' from the Mapbox map instance, casting it to the GeoJSON source type
       const mapboxSource = mapRef.current?.getSource('earthquakes') as mapboxgl.GeoJSONSource;
 
+      // Retrieves the cluster expansion zoom level for a given cluster ID using the Mapbox GeoJSON source.
+      // If successful, it updates the map view to focus on the clicked cluster, easing to the new center and zoom level.
       mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
         if (err) {
           return;
@@ -56,6 +66,8 @@ const App = () => {
 
   return (
     <div className='h-screen'>
+      {/* The Map component represents the core Mapbox GL JS instance. It renders and manages the map view
+      and interacts with various Mapbox features, such as layers and sources. */}
       <Map
         initialViewState={{
           latitude: 40.67,
@@ -68,13 +80,12 @@ const App = () => {
         onLoad={() => SpinGlobe()}
         onMouseDown={e => handleUserInteraction(e)}
         onMouseUp={e => handleUserInteraction(e)}
-        // onDragStart={e => handleUserInteraction(e)}
-        // onDragEnd={e => handleUserInteraction(e)}
         onMoveEnd={() => SpinGlobe()}
         interactiveLayerIds={['clusters']}
         onClick={e => onClick(e)}
 
       >
+        {/* The Source component represents a data source for the map. It can load and provide geographic data to be displayed on the map. Here, it loads GeoJSON data for earthquake points and clusters */}
         <Source
           id='earthquakes'
           type='geojson'
@@ -83,6 +94,7 @@ const App = () => {
           clusterMaxZoom={14}
           clusterRadius={50}
         >
+          {/* Layer components define the visual representation of data from a Source on the map. They include styling properties to control how the data is displayed. */}
           <Layer {...clusterLayer} />
           <Layer {...clusterCountLayer} />
           <Layer {...unclusteredPointLayer} />
